8b814d4cacfbdb14090d9194fa3c2d9811cad7f8
jolan <105483008+jolanValteau@users.noreply.github.com>
2024-05-24 12:05:45 +0200

    Default TODO: - Fix WalkSpeed sur la plateforme (lent) - Lerp movement

---
+++ Assets/MovingPlateform.cs
@@ -0,0 +1,65 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovingPlateform : MonoBehaviour
+{
+    public float Ratio = 1;
+    public Vector3 Axes;
+    public bool cyclicle = true;
+    public float cycleSpeed = 1;
+
+    private bool goingForward = true;
+    private float timer;
+    private List<GameObject> Riders = new();
+
+    private void Start()
+    {
+        timer = cycleSpeed;
+    }
+
+    void Update()
+    {
+        Vector3 vect = Axes * Ratio;
+        // Cyclicle
+        if (cyclicle)
+        {
+            // Timer Cycle
+            timer -= Time.deltaTime;
+            if (timer < 0)
+            {
+                goingForward = !goingForward;
+                timer = cycleSpeed;
+            } //
+
+            // gestion State Cycle
+            if (goingForward)
+                Move(vect);
+            else
+                Move(-vect); //
+
+            return;
+        }
+
+        // Not Cyclicle
+        Move(vect);
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        Riders.Add(collision.gameObject);
+    }
+
+    private void OnCollisionExit(Collision collision)
+    {
+        Riders.Remove(collision.gameObject);
+    }
+
+    private void Move(Vector3 vect)
+    {
+        transform.position += vect;
+        foreach (var go in Riders)
+        {
+            go.transform.position += vect;
+        }
+    }
+}
